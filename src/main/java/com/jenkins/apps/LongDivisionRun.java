4904ee9139d0470e60e8017e75982a6124a7afa3
rrlux <rromanov@luxoft.com>
2017-07-11 08:58:25 -0700

    rev.2

---
+++ src/main/java/com/jenkins/apps/LongDivisionRun.java
@@ -0,0 +1,15 @@
+package com.jenkins.apps;
+
+
+public class LongDivisionRun {
+
+    public static void main(String[] args) {
+
+        LongDivision division = new LongDivision();
+
+        System.out.println(division.runLongDivision("3015", "3"));
+
+    }
+
+
+}
